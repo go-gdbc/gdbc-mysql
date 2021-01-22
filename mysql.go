b600b0267d620d4cc4a3712422825fd293597b4a
Burak Köken <burakkokenn@gmail.com>
2021-01-22 23:19:54 +0200

    Completed the gdbc implementation for mysql driver. In addition, added .travis.yml.

--- mysql.go
+++ mysql.go
@@ -1,9 +1,17 @@
 package mysql
 
 import (
+	"errors"
 	"github.com/go-gdbc/gdbc"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
 )
+
+const DefaultHost = "localhost"
+const DefaultPort = "3306"
+
+const DefaultUsername = "root"
+const DefaultPassword = ""
 
 func init() {
 	gdbc.Register("mysql", "mysql", &MySQLDataSourceNameAdapter{})
@@ -14,6 +22,97 @@ type MySQLDataSourceNameAdapter struct {
 
 func (dsnAdapter MySQLDataSourceNameAdapter) GetDataSourceName(dataSource gdbc.DataSource) (string, error) {
 	dsn := ""
+	host := DefaultHost
+	port := DefaultPort
+	user := DefaultUsername
+	password := DefaultPassword
+	databaseName := ""
+
+	dataSourceUrl := dataSource.GetURL()
+
+	arguments := dataSourceUrl.Query()
+	socketArgument := ""
+	if arguments != nil {
+		socketArgument = arguments.Get("socket")
+		if socketArgument != "" {
+			user = ""
+			arguments.Del("socket")
+		}
+	}
+
+	if dataSourceUrl.Opaque != "" {
+		databaseName = dataSourceUrl.Opaque
+	} else {
+		if dataSourceUrl.Hostname() != "" {
+			host = dataSourceUrl.Hostname()
+		}
+
+		if dataSourceUrl.Port() != "" {
+			port = dataSourceUrl.Port()
+		}
+
+		if dataSourceUrl.User != nil {
+			if dataSourceUrl.User.Username() != "" {
+				user = dataSourceUrl.User.Username()
+			}
+			userPassword, _ := dataSourceUrl.User.Password()
+			if userPassword != "" {
+				password = userPassword
+			}
+		} else {
+			if dataSource.GetUsername() != "" {
+				user = dataSource.GetUsername()
+			}
+			if dataSource.GetPassword() != "" {
+				password = dataSource.GetPassword()
+			}
+		}
+
+		if dataSourceUrl.Path != "" {
+			databaseName = dataSourceUrl.Path
+		}
+	}
+
+	if strings.HasPrefix(databaseName, "/") {
+		databaseName = databaseName[1:]
+	}
+
+	if strings.Contains(databaseName, "/") {
+		return "", errors.New("database name format is wrong : " + databaseName)
+	}
+
+	if user != "" {
+		if password != "" {
+			dsn = user + ":" + password
+		} else if password == "" {
+			dsn = user
+		}
+	} else {
+		if password != "" {
+			return "", errors.New("user must not be empty when a password is specified")
+		}
+	}
+
+	if dsn != "" {
+		dsn = dsn + "@"
+	}
+
+	if socketArgument != "" {
+		dsn = dsn + "unix(" + socketArgument + ")/" + databaseName
+	} else {
+		dsn = dsn + "tcp(" + host + ":" + port + ")/" + databaseName
+	}
+
+	if len(arguments) == 0 {
+		return dsn, nil
+	}
+
+	dsn = dsn + "?"
+	for argumentName, values := range arguments {
+		dsn = dsn + argumentName + "=" + values[0] + "&"
+	}
+	dsn = dsn[:len(dsn)-1]
 
 	return dsn, nil
+
 }
--- mysql_test.go
+++ mysql_test.go
@@ -1,1 +1,83 @@
 package mysql
+
+import (
+	"github.com/go-gdbc/gdbc"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func getDSN(t *testing.T, dataSourceUrl string) (string, error) {
+	adapter := gdbc.GetDataSourceNameAdapter("mysql")
+	dataSource, err := gdbc.GetDataSource(dataSourceUrl)
+	assert.Nil(t, err)
+	return adapter.GetDataSourceName(dataSource)
+}
+
+func getDSNWithUser(t *testing.T, dataSourceUrl string, username string, password string) (string, error) {
+	adapter := gdbc.GetDataSourceNameAdapter("mysql")
+	dataSource, err := gdbc.GetDataSource(dataSourceUrl, gdbc.Username(username), gdbc.Password(password))
+	assert.Nil(t, err)
+	return adapter.GetDataSourceName(dataSource)
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithWrongDatabaseName(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql://localhost:3000/testdb/wrong?charset=utf8mb4")
+	assert.NotNil(t, err)
+	assert.Equal(t, "database name format is wrong : testdb/wrong", err.Error())
+	assert.Empty(t, dsn)
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUser(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql://localhost:3000/testdb?charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, DefaultUsername+"@tcp(localhost:3000)/testdb?charset=utf8mb4")
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUserAndPort(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql://localhost/testdb?charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, DefaultUsername+"@tcp(localhost:"+DefaultPort+")/testdb?charset=utf8mb4")
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUserAndHostAndPort(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql:testdb?charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, DefaultUsername+"@tcp("+DefaultHost+":"+DefaultPort+")/testdb?charset=utf8mb4")
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUserAndHostAndPortAndDatabase(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql:?charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, DefaultUsername+"@tcp("+DefaultHost+":"+DefaultPort+")/?charset=utf8mb4")
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithFullFormat(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql://username:password@localhost:3000/testdb?charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, "username:password@tcp(localhost:3000)/testdb?charset=utf8mb4")
+}
+
+func TestPostgresDataSourceNameAdapter_GetDataSourceNameWithSocket(t *testing.T) {
+	dsn, err := getDSN(t, "gdbc:mysql:/testdb?socket=/tmp/mysql.sock&charset=utf8mb4")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, "unix(/tmp/mysql.sock)/testdb?charset=utf8mb4")
+}
+
+func TestPostgresDataSourceNameAdapter_GetDataSourceNameWithSocketUsingUserAndPassword(t *testing.T) {
+	dsn, err := getDSNWithUser(t, "gdbc:mysql:/testdb?socket=/tmp/mysql.sock&charset=utf8mb4", "username", "password")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, "username:password@unix(/tmp/mysql.sock)/testdb?charset=utf8mb4")
+}
+
+func TestPostgresDataSourceNameAdapter_GetDataSourceNameWithSocketUsingUser(t *testing.T) {
+	dsn, err := getDSNWithUser(t, "gdbc:mysql:/testdb?socket=/tmp/mysql.sock&charset=utf8mb4", "username", "")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, "username@unix(/tmp/mysql.sock)/testdb?charset=utf8mb4")
+}
+
+func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUsernameUsingSocket(t *testing.T) {
+	dsn, err := getDSNWithUser(t, "gdbc:mysql:/testdb?socket=/tmp/mysql.sock&charset=utf8mb4", "", "test")
+	assert.NotNil(t, err)
+	assert.Equal(t, "user must not be empty when a password is specified", err.Error())
+	assert.Empty(t, dsn)
+}
